d00069829929e2e23bf1546225ff142e4d79de3f
test <test>
2022-03-10 16:20:42 -0500

    added the enemy bots and got all the loop for the rounds set

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,20 +7,20 @@ var playerHealth = 100;
 var playerAttack = 10; 
 var playerMoney = 10;
 
-console.log("Name: " + playerName + ", Player attack pts: " + playerAttack + ", Health: " + playerHealth + " pts: " + playerMoney);
+// console.log("Name: " + playerName + ", Player attack pts: " + playerAttack + ", Health: " + playerHealth + " pts: " + playerMoney);
 
 var enemyNames = [" Roboto", " Amy Android", " Robo Trumble"];
 var enemyHealth = 50; 
 var enemyAttack = 12; 
-console.log("Here are your enemys names:" + enemyNames); 
+// console.log("Here are your enemys names:" + enemyNames); 
 
-for (var i = 0; i < enemyNames.length; i++){
-    console.log(enemyNames[i]);
-    console.log(i);
-    console.log(enemyNames[i] + " is at " + i + " index");
-}
+// for (var i = 0; i < enemyNames.length; i++){
+//     console.log(enemyNames[i]);
+//     console.log(i);
+//     console.log(enemyNames[i] + " is at " + i + " index");
+// }
 
-var fight = function() {
+var fight = function(enemyName) {
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     
@@ -33,19 +33,19 @@ var fight = function() {
         // remove enemy's health by subtracting the amount set in the playerAttack variable 
         enemyHealth = enemyHealth - playerAttack; 
         console.log(
-            playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
+            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
         // check enemy's health
         if (enemyHealth <= 0) {
-            window.alert(enemyNames + " has died!");
+            window.alert(enemyName + " has died!");
         } else {
-            window.alert(enemyNames + " stll has " + enemyHealth + " health left.");
+            window.alert(enemyName + " stll has " + enemyHealth + " health left.");
         }
         
         // remove player's health by subtracting the amount set in the enemyAttack variable
         playerHealth = playerHealth - enemyAttack;
         console.log(
-            enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
 
         // checking players health 
@@ -61,6 +61,7 @@ var fight = function() {
         // if yes (true), leave fight
         if (confirmSkip === "y" || confirmSkip === "Y") {
             window.alert(playerName + " has decided to skip this fight.")
+            console.log(playerName + " has decided to skip this fight.")
             // subtract player money 
             playerMoney = playerMoney - 2;
             console.log(
@@ -70,8 +71,7 @@ var fight = function() {
 
         // if no (false), ask question again by running fight() again
         else if (confirmSkip === "n" || confirmSkip === "N") {
-
-            fight();
+                fight(enemyNames[i]);
         }
 
     } else {
@@ -79,5 +79,7 @@ var fight = function() {
     }
 }
 
-// fight();
-// console.timeEnd('abc');
+for (var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
+console.timeEnd('abc');
