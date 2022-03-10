c6c492547d260bbd89c37b21559a41ea7cc3b6ed
test <test>
2022-03-09 23:16:50 -0500

    got doen with making a round of the game with a few extra lines jsut for my own sake; making a save point aswell

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,13 @@
 
 
-
+console.time('abc');
 
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
-var playerAttack = 10 
+var playerAttack = 10; 
+var playerMoney = 10;
 
-console.log("Name: " + playerName + ", Player attack pts: " + playerAttack + ", Health: " + playerHealth);
+console.log("Name: " + playerName + ", Player attack pts: " + playerAttack + ", Health: " + playerHealth + " pts: " + playerMoney);
 
 var enemyName = 'Roboto';
 var enemyHealth = 50; 
@@ -18,6 +19,10 @@ var fight = function() {
     
     // if player chooses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
+        // reward player for not skipping 
+        playerMoney = playerMoney + 1;
+        window.alert("You've been awarded +1 pts for not skipping, congrats!");
+        console.log(playerName + " Now has " + playerMoney + ".")
         // remove enemy's health by subtracting the amount set in the playerAttack variable 
         enemyHealth = enemyHealth - playerAttack; 
         console.log(
@@ -45,10 +50,27 @@ var fight = function() {
 
         // if player chooses to skip
     } else if (promptFight === "skip" || promptFight === "SKIP") {
-        window.alert (playerName + " has chosen to skip the fight");
+        var confirmSkip = window.prompt("Are you sure you'd like to skip, type Y or N.");
+        // if yes (true), leave fight
+        if (confirmSkip === "y" || confirmSkip === "Y") {
+            window.alert(playerName + " has decided to skip this fight.")
+            // subtract player money 
+            playerMoney = playerMoney - 2;
+            console.log(
+                playerName + " now has " + playerMoney + " pts left. -2 points for skipping round."
+            )
+        }
+
+        // if no (false), ask question again by running fight() again
+        else if (confirmSkip === "n" || confirmSkip === "N") {
+
+            fight();
+        }
+
     } else {
         window.alert ("You need to choose a valid option, please try again");
     }
 }
 
 fight();
+console.timeEnd('abc');
